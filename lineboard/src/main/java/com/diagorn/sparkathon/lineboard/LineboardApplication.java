2c3609790d8bf4d00f3fb4f241d1bc3873bfadee
diagorn <mikhail.gasin@fors.ru>
2025-08-21 15:43:59 +0300

    added initial starters

---
+++ lineboard/src/main/java/com/diagorn/sparkathon/lineboard/LineboardApplication.java
@@ -0,0 +1,11 @@
+package com.diagorn.sparkathon.lineboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LineboardApplication.class, args);
+    }
+}
